064327743330419df3fabf3a9a31371b5dcc7c08
Oleksandr Yeremenko <oyeremenko@divante.pl>
2016-11-24 03:17:13 +0100

    Init magento configuration

---
+++ config/env.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# This file contains environments variables
+# that are required for the installation of
+# Magento.
+
+export DATABASE_NAME="magento"
+export DATABASE_USER="magento"
+export DATABASE_PASSWORD="enAVINa2"
+export DATABASE_ROOT_PASSWORD="enAVINa2"
+
+export ADMIN_USERNAME="admin"
+export ADMIN_FIRSTNAME="Admin"
+export ADMIN_LASTNAME="Admin"
+export ADMIN_EMAIL="admin@example.com"
+export ADMIN_PASSWORD="enAVINa2"
+
+export DEFAULT_LANGUAGE="en_US"
+export DEFAULT_CURRENCY="EUR"
+export DEFAULT_TIMEZONE="Europe/Warsaw"
+
+export BACKEND_FRONTNAME="management"
